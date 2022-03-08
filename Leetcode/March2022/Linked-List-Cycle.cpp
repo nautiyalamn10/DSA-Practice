113d186c15dffd06ea9ea87722d695b1c79ef12c
Aman Nautiyal <59726867+nautiyalamn10@users.noreply.github.com>
2022-03-08 18:32:10 +0530

    Create Linked-List-Cycle.cpp

---
+++ Leetcode/March2022/Linked-List-Cycle.cpp
@@ -0,0 +1,11 @@
+bool hasCycle(ListNode *head) {
+    ListNode* fast=head,*slow=head;
+    while(fast&&fast->next){
+        fast=fast->next->next;
+        slow=slow->next;
+        if(slow==fast){
+            return true;
+        }
+    }
+    return false;
+}
